fff4074c13b0a7f4f8ffd5f732a1f4f74fda02e6
Yevhenii Hyzyla <y.hyzyla@smartweb.com.ua>
2020-04-26 16:13:35 +0300

    Update routes

--- app/handlers.py
+++ app/handlers.py
@@ -2,7 +2,12 @@ from app import app, dispatcher
 from app.utils import get_update_from_request
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
+def index():
+    return 'OK'
+
+
+@app.route('/bot', methods=['GET', 'POST'])
 def bot_handler():
     update = get_update_from_request()
     dispatcher.process_update(update)
